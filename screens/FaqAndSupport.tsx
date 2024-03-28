c5d04e8386d55f5ec9baef79861f2ab030f85d19
monank sojitra <monaksojitra4444@gmail.com>
2024-03-28 22:22:02 +0530

    [add]: setup Accordion in faq component

--- screens/FaqAndSupport.tsx
+++ screens/FaqAndSupport.tsx
@@ -1,12 +1,72 @@
 import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import React, {useState} from 'react';
 import Header from '../components/Header';
-
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Accordion from 'react-native-collapsible/Accordion';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const FaqAndSupport = () => {
-  const [tab, setTab] = useState(false);
+// Define the Section type
+type Section = {
+  title: string;
+  content: JSX.Element;
+};
+
+const FaqAndSupport: React.FC = () => {
+  const [tab, setTab] = useState<boolean>(false);
+  const [activeSections, setActiveSections] = useState<number[]>([]);
+
+  const sections: Section[] = [
+    {
+      title: 'What is Rise Real Estate?',
+      content: (
+        <Text>
+          React Native lets you create truly native apps and doesn't compromise
+          your users' experiences.
+        </Text>
+      ),
+    },
+    {
+      title: 'Why choose buy in Rise?',
+      content: (
+        <Text>
+          See your changes as soon as you save. With the power of JavaScript,
+          React Native lets you iterate at lightning speed.
+        </Text>
+      ),
+    },
+    {
+      title: 'What is Safar?',
+      content: (
+        <Text>
+          See your changes as soon as you save. With the power of JavaScript,
+          React Native lets you iterate at lightning speed.
+        </Text>
+      ),
+    },
+  ];
+
+  function renderHeader(section: Section, _: unknown, isActive: boolean) {
+    return (
+      <View className="flex flex-row items-center justify-between p-2">
+        <Text className="text-blue-200 text-base font-bold">
+          {section.title}
+        </Text>
+        <Icon name={isActive ? 'minus' : 'plus'} size={20} color="#8BC83F" />
+      </View>
+    );
+  }
+
+  function renderContent(section: Section, _: unknown, _isActive: boolean) {
+    return (
+      <View className="bg-[#F5F4F8] rounded-xl p-3 tracking-wider leading-relaxed">
+        <Text className="text-blue-200 text-xs font-normal">
+          {section.content}
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View className="h-full bg-white">
       <Header />
@@ -41,7 +101,7 @@ const FaqAndSupport = () => {
       <View className="flex flex-row items-center p-4 m-5 bg-light-200 rounded-xl">
         <Fontisto name="search" size={20} color="#252B5C" />
         <TextInput
-          className=" w-full px-2"
+          className=" w-full px-2 text-blue-300"
           inputMode="search"
           placeholderTextColor="#A1A5C1"
           placeholder="Try find “how to”"
@@ -49,7 +109,7 @@ const FaqAndSupport = () => {
       </View>
       <View className="bg-light-200 flex flex-row items-center justify-around mx-5 h-14 rounded-xl">
         <TouchableOpacity
-          onPress={() => setTab(!tab)}
+          onPress={() => setTab(false)}
           className={`${
             !tab ? 'bg-white' : ''
           } flex h-10 items-center justify-center flex-1 mx-3 rounded-3xl`}>
@@ -61,7 +121,7 @@ const FaqAndSupport = () => {
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => setTab(!tab)}
+          onPress={() => setTab(true)}
           className={`${
             tab ? 'bg-white' : ''
           } flex h-10 items-center justify-center flex-1 mx-3 rounded-3xl`}>
@@ -72,6 +132,17 @@ const FaqAndSupport = () => {
             Estate Agent
           </Text>
         </TouchableOpacity>
+      </View>
+      <View className="mx-6 mt-3">
+        <Accordion
+          align="bottom"
+          sections={sections}
+          activeSections={activeSections}
+          renderHeader={renderHeader}
+          renderContent={renderContent}
+          onChange={sections => setActiveSections(sections)}
+          underlayColor={'#F5F4F8'}
+        />
       </View>
     </View>
   );
